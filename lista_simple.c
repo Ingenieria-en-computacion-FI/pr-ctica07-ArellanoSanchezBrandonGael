c2410d3b9b6e2ad482b4ff0a303d1f63b679177b
ArellanoSanchezBrandonGael <sanchezbrandonk123@gmail.com>
2026-03-26 09:45:16 -0600

    Update lista_simple.c

--- lista_simple.c
+++ lista_simple.c
@@ -1,1 +1,16 @@
+#include "lista_simple.h"
 
+Nodo * crearNodo(void *dato, size_t size) {
+  Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
+  if(nuevo != NULL){
+    nuevo ->siguiente = NULL;
+    nuevo->dato = malloc(size);
+    mempcy(nuevo->dato, dato, size); //copia memoria, memory copy
+  }
+  return nuevo;
+}
+void borrarNodo(Nodo *n);
+    if(nuevo != NULL){
+      free(nuevo->dato);
+
+bool modificarNodo(Nodo *n, void *d);
